5d434fabc3eea2f270e19697597fc4352fb3c3d2
mandywl <Mengsi.Wells-Lakeland@vu.edu.au>
2020-07-12 00:00:50 +1000

    Created shopping list db

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const recipeRoutes = require("./recipeRoute");
+const shoppingRoutes = require("./shoppingRoute");
 
 // Post routes
 router.use("/recipes", recipeRoutes);
+router.use("/shoppinglist", shoppingRoutes);
 
 module.exports = router;
---
+++ routes/api/shoppingRoute.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const shoppingController = require("../../controllers/shoppingController");
+
+// Matches with "/api/posts"
+router.route("/").post(shoppingController.create);
+
+router.route("/").get(shoppingController.findAll);
+
+// Matches with "/api/posts/:id"
+router
+  .route("/:id")
+  .get(shoppingController.findById)
+  .delete(shoppingController.remove);
+
+module.exports = router;
