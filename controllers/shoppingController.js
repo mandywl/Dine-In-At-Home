801f4533d7139db2bbd661698525e55086ace2fa
mandywl <Mengsi.Wells-Lakeland@vu.edu.au>
2020-07-20 18:10:29 +1000

    Formated Code

--- controllers/shoppingController.js
+++ controllers/shoppingController.js
@@ -2,17 +2,17 @@ const db = require("../models");
 
 // Defining methods for the postsController
 module.exports = {
-  findAll: function (req, res) {
+  findAll(req, res) {
     db.ShoppingList.find(req.query)
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
-  findById: function (req, res) {
+  findById(req, res) {
     db.ShoppingList.findById(req.params.id)
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
-  create: async function (req, res) {
+  async create(req, res) {
     const item = await db.ShoppingList.find(req.body);
     if (!item.length) {
       db.ShoppingList.create(req.body)
@@ -22,23 +22,23 @@ module.exports = {
       res.status(200).send("");
     }
   },
-  update: function (req, res) {
+  update(req, res) {
     db.ShoppingList.findOneAndUpdate({ _id: req.params.id }, req.body)
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
-  remove: function (req, res) {
+  remove(req, res) {
     db.ShoppingList.findById({ _id: req.params.id })
       .then((dbModel) => dbModel.remove())
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
-  find: function (req, res) {
+  find(req, res) {
     db.ShoppingList.find({ ingrediates: req.params.ingrediates })
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
-  findByUserId: function (req, res) {
+  findByUserId(req, res) {
     db.ShoppingList.find({ userID: req.params.id })
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
