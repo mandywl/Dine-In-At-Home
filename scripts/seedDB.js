49528f435318c3665070711f1aab32408728c452
mandywl <Mengsi.Wells-Lakeland@vu.edu.au>
2020-07-20 16:51:04 +1000

    Corrected seeds

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -101,7 +101,21 @@ const recipeSeed = [
     method: [
       {
         first:
-          "1. Preheat over to high grill. Set aside a mediun baking dish. Bring two medium pots of salted water to the boil. Dice kumamra and potatoes 2cm. 2. Cook kumara and potatoes in first pot of boiling water for 15-20 minutes, until sorf. Drain and mash with first measure of butter and milk. season to taste and set aside. 3. While kumara and potatoes are cooking, finely dice onion and dice carrots 1cm. Heat a little oil in a medium fry-pan on high heat. 4. Cook onion, carrots and salt for 3 minutes, until starting to brown. Add beef and garlic and cook for about 7 minutes, breaking mince up as it cooks, until browned. 5. Reduce heat to medium, add pie herbs and flour and cook a further 1 minute, stirring until fragrant. Add stock, Worcestershire sauce, soy sauce and tomato sauce. Bring to a simmer and cook for about 3 minutes =, until thickended. Season to taste. Transfer to reserved dish. Spoon mash on top of mince, spreading out to cover. sprinkle over cheese and grill on middle upper oven rack for 5-7 minutes, until cheese is golden. Cheesy Goodness. 7. While pie is grilling, cut broccoli into small florets. Add to second pot of boiling water along with peas and cook about 4 minutes, until bright green and tender. Drain and return to pot with second measure of butter. toss to combine and season to taste. 8. To serve, divid pie between plates and serve greens on the side. Serve with tomato sauce. ",
+          "1. Preheat over to high grill. Set aside a mediun baking dish. Bring two medium pots of salted water to the boil. Dice kumamra and potatoes 2cm.",
+        second:
+          "2. Cook kumara and potatoes in first pot of boiling water for 15-20 minutes, until sorf. Drain and mash with first measure of butter and milk. season to taste and set aside.",
+        third:
+          "3. While kumara and potatoes are cooking, finely dice onion and dice carrots 1cm. Heat a little oil in a medium fry-pan on high heat.",
+        fourth:
+          "4. Cook onion, carrots and salt for 3 minutes, until starting to brown. Add beef and garlic and cook for about 7 minutes, breaking mince up as it cooks, until browned.",
+        fifth:
+          "5. Reduce heat to medium, add pie herbs and flour and cook a further 1 minute, stirring until fragrant. Add stock, Worcestershire sauce, soy sauce and tomato sauce. Bring to a simmer and cook for about 3 minutes =, until thickended. Season to taste.",
+        sixth:
+          "6. Transfer to reserved dish. Spoon mash on top of mince, spreading out to cover. sprinkle over cheese and grill on middle upper oven rack for 5-7 minutes, until cheese is golden. Cheesy Goodness.",
+        seventh:
+          "7. While pie is grilling, cut broccoli into small florets. Add to second pot of boiling water along with peas and cook about 4 minutes, until bright green and tender. Drain and return to pot with second measure of butter. toss to combine and season to taste.",
+        eighth:
+          "8. To serve, divid pie between plates and serve greens on the side. Serve with tomato sauce. ",
       },
     ],
   },
@@ -130,7 +144,13 @@ const recipeSeed = [
     method: [
       {
         first:
-          "1. Prep Tacos. PREHEAT OVEN TO 200C. Wrap torillas in fiol and place in oven to warm for about 20 minutes, until warmed through. Keep wrapped in foil unitl ready to serve. 2. Cook Bean Smash. Rinse and drain kidney beans and place all bean smash ingredients in pot on low heat. bring to a simmer and cook for 3-4 minutes, until warmed through. Addoil and mash until smooth. Season to taste and cover to keep warn. 3. Prep Salad and Chicken. add ranchslaw to a blow with chipotle, first measure of sour cream, mayo and vinegar. Mix to combine, season to taste and set aside. Rub chicken with first measure of taco spice and season with salt. 4. Cook Chicken. Heat a little oil in a large fry-pan on high heat and cook chicken of 4 minutes each side (depending on thickness) until cooked through. Rest, covered on plate for 3 minutes. Thinly slice and toss back through resting juices.",
+          "1. Prep Tacos. PREHEAT OVEN TO 200C. Wrap torillas in fiol and place in oven to warm for about 20 minutes, until warmed through. Keep wrapped in foil unitl ready to serve.",
+        second:
+          "2. Cook Bean Smash. Rinse and drain kidney beans and place all bean smash ingredients in pot on low heat. bring to a simmer and cook for 3-4 minutes, until warmed through. Addoil and mash until smooth. Season to taste and cover to keep warn.",
+        thrid:
+          "3. Prep Salad and Chicken. add ranchslaw to a blow with chipotle, first measure of sour cream, mayo and vinegar. Mix to combine, season to taste and set aside. Rub chicken with first measure of taco spice and season with salt. ",
+        fourth:
+          "4. Cook Chicken. Heat a little oil in a large fry-pan on high heat and cook chicken of 4 minutes each side (depending on thickness) until cooked through. Rest, covered on plate for 3 minutes. Thinly slice and toss back through resting juices.",
       },
     ],
   },
@@ -160,7 +180,17 @@ const recipeSeed = [
     method: [
       {
         first:
-          "1. Combine rice, water and salt in a medium pot and bring to the boil. As soon as it boils, cover with a lid and reduce to lowest heat to cook for 12 minutes. turn off heat and leave to steam, still covered, for a further 8 minutes. Do not lift lid during cooking. 2. Pat Chicken dry and dice 3cm, add to bowl along with chicken flour and salt. Toss well to combine. Heat a little oil in a large fry-pan on high heat. 3. Shake off excess flour and cook chicken, in two batches, for 5-6 minutes, until cooked through and golden. Turn occasionally to cook on all sides. Wipe pan clean between batches and add a little more oil as needed. Set aside an paer towels and season with salt. 4. Thinly slice cabbage until you have 3 cups worth; thinly slice spring onions; grate carrots and courgette. Add all to a large bowl and set aside. 5. When chicken is cooked add all remaininh slaw ingredients to the bowl, mix to combine and season to taste. 6. In a small bowl, combine second measure of mayo and semame oil. 7. to serve, spoon rice onto plates, top with crispy chicken and serve slaw on the side. Dollop with mayo mix. ",
+          "1. Combine rice, water and salt in a medium pot and bring to the boil. As soon as it boils, cover with a lid and reduce to lowest heat to cook for 12 minutes. turn off heat and leave to steam, still covered, for a further 8 minutes. Do not lift lid during cooking.",
+        second:
+          "2. Pat Chicken dry and dice 3cm, add to bowl along with chicken flour and salt. Toss well to combine. Heat a little oil in a large fry-pan on high heat.",
+        third:
+          "3. Shake off excess flour and cook chicken, in two batches, for 5-6 minutes, until cooked through and golden. Turn occasionally to cook on all sides. Wipe pan clean between batches and add a little more oil as needed. Set aside an paer towels and season with salt.",
+        fourth:
+          "4. Thinly slice cabbage until you have 3 cups worth; thinly slice spring onions; grate carrots and courgette. Add all to a large bowl and set aside.",
+        fifth:
+          "5. When chicken is cooked add all remaininh slaw ingredients to the bowl, mix to combine and season to taste.",
+        sixth:
+          "6. In a small bowl, combine second measure of mayo and semame oil. 7. to serve, spoon rice onto plates, top with crispy chicken and serve slaw on the side. Dollop with mayo mix.",
       },
     ],
   },
@@ -192,7 +222,19 @@ const recipeSeed = [
     method: [
       {
         first:
-          "1. Cook Eggs, Bring a small pot of water and a large pot of salted water to the boil, gently add eggs to the small pot. Cook for 10 minutes hard-boiled. Drain and refresh under cold water. 2. Prep and cook pork. Pat dry, add to a medium bowl and mix with all remaining pork ingredientts. Heat a little oil in a large, deep fry-pan on medium heat. lift pork out of marinade and cook for 3 minutes each side. until cooked through. Set aside, covered to rest. 3. Prep Veggies. Cut carrots in half lengthways then thinly slice. Thinly slice bok choy. 4. Cook Broth. retune same pan to medium -high heat with a little more oil and cook carrots for about 3 minutes, until softened. Add corn and cook a further 2 minutes, until fragrant. Add Broth spices, water, stock and soy sause. Bring to a simmer and remove from heat.Stir in bok choy and season to taste with soy sauce. 5. Cook Noodles. Cook noodles in larfe pot of boiling water for about 5 minutes, stirring to separate noodles, until tender. drrain and return to pot with a little oil, to prevent sticking. Cover and set aside. Finish Eggs and pork. Peel aggs and cut in half. Thinly slice pork and add any resting juices to the broth. 7. To serve. Dived noodles between bowls, ladle over broth and veggies, then top with pork and eggs.     ",
+          "1. Cook Eggs, Bring a small pot of water and a large pot of salted water to the boil, gently add eggs to the small pot. Cook for 10 minutes hard-boiled. Drain and refresh under cold water.",
+        second:
+          "2. Prep and cook pork. Pat dry, add to a medium bowl and mix with all remaining pork ingredientts. Heat a little oil in a large, deep fry-pan on medium heat. lift pork out of marinade and cook for 3 minutes each side. until cooked through. Set aside, covered to rest.",
+        thrid:
+          "3. Prep Veggies. Cut carrots in half lengthways then thinly slice. Thinly slice bok choy.",
+        fourth:
+          "4. Cook Broth. retune same pan to medium -high heat with a little more oil and cook carrots for about 3 minutes, until softened. Add corn and cook a further 2 minutes, until fragrant. Add Broth spices, water, stock and soy sause. Bring to a simmer and remove from heat.Stir in bok choy and season to taste with soy sauce.",
+        fifth:
+          "5. Cook Noodles. Cook noodles in larfe pot of boiling water for about 5 minutes, stirring to separate noodles, until tender. drain and return to pot with a little oil, to prevent sticking. ",
+        sixth:
+          "6. Cover and set aside. Finish Eggs and pork. Peel aggs and cut in half. Thinly slice pork and add any resting juices to the broth.",
+        seventh:
+          "7. To serve. Dived noodles between bowls, ladle over broth and veggies, then top with pork and eggs.",
       },
     ],
   },
