1e0a0a6b49afb50c823532c5e9af000f4a5711e1
mandywellslakeland <mandy.wells-lakeland@orygen.org.au>
2023-07-16 02:44:45 +1000

    Added build directory

---
+++ client/build/precache-manifest.f5ef21d3eaa04f23ee222a595d6f6a18.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "76cb77a9d75f74fba06e",
+    "url": "/static/css/main.91ef4dd7.chunk.css"
+  },
+  {
+    "revision": "76cb77a9d75f74fba06e",
+    "url": "/static/js/main.05134bc6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "68fdb4069ab1a3beb3e4",
+    "url": "/static/css/2.1dcf44d3.chunk.css"
+  },
+  {
+    "revision": "68fdb4069ab1a3beb3e4",
+    "url": "/static/js/2.e5d821fa.chunk.js"
+  },
+  {
+    "revision": "e6ed01886d87057b1065e4be29de99c1",
+    "url": "/static/media/beefStroganoff.e6ed0188.jpg"
+  },
+  {
+    "revision": "866a96074174e0a4a3cdbd3c54d62411",
+    "url": "/static/media/honeySoyChicken.866a9607.jpg"
+  },
+  {
+    "revision": "def318d59690c3bcd662c396de5dec01",
+    "url": "/static/media/bbqPorkRamen.def318d5.jpg"
+  },
+  {
+    "revision": "b542cc08b6a1404883b6aff48649f112",
+    "url": "/static/media/beefNCheesePie.b542cc08.jpg"
+  },
+  {
+    "revision": "3549bba34fa3de722fd33d36e09a2fe4",
+    "url": "/static/media/chickenTaco.3549bba3.jpg"
+  },
+  {
+    "revision": "85154b84a4dd045188d7bead7595f872",
+    "url": "/static/media/crispyJapaneseChicken.85154b84.jpg"
+  },
+  {
+    "revision": "73b2021c8e99d913f712c067b8598afa",
+    "url": "/static/media/teriyakiBeef.73b2021c.jpg"
+  },
+  {
+    "revision": "6648171cf214ef63f75d12e8567e269a",
+    "url": "/static/media/fishCurry.6648171c.jpg"
+  },
+  {
+    "revision": "f7676ae6278a97ba1f628846695789e7",
+    "url": "/static/media/beefRumpSteaks.f7676ae6.jpg"
+  },
+  {
+    "revision": "e1a28be064b5bd0f13f334dfda40165d",
+    "url": "/static/media/logo.e1a28be0.png"
+  },
+  {
+    "revision": "f2d11eecb5ae30d7a8f06699740b455c",
+    "url": "/static/media/macNCheese.f2d11eec.jpeg"
+  },
+  {
+    "revision": "fc7a9ae732fc12e89f0f8af06d3ce0bf",
+    "url": "/static/media/chocBrownies.fc7a9ae7.jpg"
+  },
+  {
+    "revision": "451c7a8807c330262cdaa431de346fa7",
+    "url": "/static/media/homepage_bg.451c7a88.png"
+  },
+  {
+    "revision": "0d497f623ad99c4af148ccec6c4c6f24",
+    "url": "/index.html"
+  }
+];
